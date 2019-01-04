52a28eae1f6f69ca9d5cfdc729c4a1c6b44bee83
diana807 <Dy1rah2d>
2019-01-04 17:15:00 +0300

    реализованы методы из ТЗ

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (get(r.uuid)==null) {
+            storage[size()] = r;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                r = storage[i];
+            }
+            break;
+        }
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                while(i<size()-1){
+                    storage[i]=storage[i++];
+                }
+                storage[i]=null;
+            }
+            break;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumesNotNull = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            resumesNotNull[i] = storage[i];
+        }
+        return resumesNotNull;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume r : storage) {
+            if (r != null) count++;
+            else break;
+        }
+        return count;
     }
 }
+
