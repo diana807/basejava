ab0a58b41d8651005fa8ed63300d4e1a33fc7cb1
diana807 <Dy1rah2d>
2019-03-21 22:01:16 +0300

    HW3 - ready to HW4

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,83 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractArrayStorageTest {
+
+    private Storage storage = new ArrayStorage();
+    public static final String UUID_1 = "uuid1";
+    public static final String UUID_2 = "uuid2";
+    public static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExist() {
+
+    }
+
+    @Test
+    public void save() {
+        Resume newResume = new Resume("uuid4");
+        storage.save(newResume);
+        Assert.assertEquals(newResume, storage.get("uuid4"));
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExist() {
+        storage.save(new Resume("uuid1"));
+    }
+
+    @Test(expected = StorageException.class)
+    public void storageOverflow() {
+        for (int i = 0; i < 10_000; i++)
+            storage.save(new Resume());
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() {
+        storage.delete("uuid5");
+    }
+}
