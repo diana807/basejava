0d535a1e056a96d68116d5d401cf4c7c9b6cf1de
diana807 <Dy1rah2d>
2019-03-21 22:53:10 +0300

    HW4 - reflection and test classes

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,13 +1,8 @@
 package com.urise.webapp.storage;
 
-import static org.junit.Assert.*;
-
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
-    Storage arrayStorage = new ArrayStorage();
-
-    public ArrayStorageTest(Storage storage) {
-        super(storage);
-        this.arrayStorage = storage;
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
     }
 }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,12 +1,8 @@
 package com.urise.webapp.storage;
 
-import static org.junit.Assert.*;
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
 
-public class SortedArrayStorageTest extends AbstractArrayStorageTest{
-
-    public Storage sortedArrayStorage = new SortedArrayStorage();
-    public SortedArrayStorageTest(Storage storage) {
-        super(storage);
-        this.sortedArrayStorage = storage;
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
     }
 }
